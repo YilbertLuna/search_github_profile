1cc772e6dd4115b0e791bac9dfbd92a3b282e8db
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-12-09 16:52:29 -0400

    file to get data users

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+const API_URL = "https://api.github.com/users"
+
+export async function getDataIndoUsera(user) {
+    try {
+        const res = await fetch(`${API_URL}/${user}`)
+        const data = await res.json()
+        console.log(data)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
