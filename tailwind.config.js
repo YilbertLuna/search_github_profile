c41929106c206e6d4244ff324399d9b93e8c92f1
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-12-09 20:05:11 -0400

    modified file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './msrc/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,dx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors:{
-      'primary': '#b1d4e0',
-      'secondary': '#145da0',
-      'tertiary': '#56aeff'
+    extend: {
+      colors:{
+        'primary': '#b1d4e0',
+        'secondary': '#145da0',
+        'tertiary': '#56aeff'
+      }
     }
   },
   plugins: [],
