e2f605e16af80a4165595935ca894cd5a17bae47
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-12-10 01:14:55 -0400

    is a user doesnt exist does not return data

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,10 @@ export async function getDataIndoUsera(user) {
     try {
         const res = await fetch(`${API_URL}/${user}`)
         const data = await res.json()
+        console.log(res.status)
         console.log(data)
-        return data
+        if(res.status === 200) return data
+        // return data
     } catch (error) {
         console.log(error)
     }
